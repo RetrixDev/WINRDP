a345dd1d7c6af515e75f6c2d1b83095a4d3cead8
Lokesh <77056553+RetrixDev@users.noreply.github.com>
2021-03-23 17:29:00 +0530

    Update and rename run to run.py

---
+++ run.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+webbrowser.open('http://example.com')  
+webbrowser.open('https://redirector.gvt1.com/edgedl/android/studio/install/4.1.3.0/android-studio-ide-201.7199119-windows.exe') 
