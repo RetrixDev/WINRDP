6b5bf2fb570eb671afea0f389bc22f9727392c0b
Lokesh <77056553+RetrixDev@users.noreply.github.com>
2021-03-23 18:06:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-import webbrowser
+import subprocess
+import sys
 
-webbrowser.open('http://example.com')  
-webbrowser.open('https://redirector.gvt1.com/edgedl/android/studio/install/4.1.3.0/android-studio-ide-201.7199119-windows.exe') 
+try:
+    import selenium as pd
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'selenium'])
+finally:
+    from selenium import webdriver
+    driver = webdriver.Chrome()
+    driver.get("https://redirector.gvt1.com/edgedl/android/studio/install/4.1.3.0/android-studio-ide-201.7199119-windows.exe")
+    driver.get("https://github.com/RetrixDev/mirrorbot-workflow/blob/main/installbackupandsync.exe?raw=true")
+
+
