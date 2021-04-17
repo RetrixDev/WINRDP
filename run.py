5af8449582a8f7c4d345122e9b2af8c840ec52c4
Lokesh <77056553+RetrixDev@users.noreply.github.com>
2021-04-17 06:49:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ except ImportError:
 finally:
     from selenium import webdriver
     driver = webdriver.Chrome()
-#     driver.get("https://fglink.xyz/G-Tv.zip")
+   
+    driver.get("https://redirector.gvt1.com/edgedl/android/studio/install/4.1.3.0/android-studio-ide-201.7199119-windows.exe")
 
     driver.get("https://cdn-fastly.obsproject.com/downloads/OBS-Studio-26.1.1-Full-Installer-x64.exe")
 
